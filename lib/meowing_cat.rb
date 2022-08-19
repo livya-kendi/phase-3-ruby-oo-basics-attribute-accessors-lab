2dda7b3e0531c1ba4738ccc891541183f0c37136
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-08-19 03:27:04 +0300

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+
+class Cat
+    attr_accessor :name
+
+    def meow=(name)
+        @name=name
+    end
+
+    def meow
+        @name
+        puts "meow!"
+    end
+end
+
